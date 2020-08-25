2f65e43d8deb956a96fc8ef01082bce27857a430
mariana <luzmarianacarlo1999@gmail.com>
2020-08-24 23:26:45 -0400

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
+import { AppRouting } from './app.routing';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TareaComponent } from './tarea/tarea.component';
 import { FormsModule } from '@angular/forms';
 import { NgColorDirective } from './directives/ng-color.directive';
 import { NgFocusDirective } from './directives/ng-focus.directive';
@@ -21,8 +22,9 @@ import { AboutComponent } from './components/about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule, 
-    FormsModule
+    BrowserModule,
+    FormsModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]

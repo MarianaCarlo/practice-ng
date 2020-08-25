ff2b3a974dc30b10d9ee345bc8ae97f2145a77ee
MarianaCarlo <luzmarianacarlo1999@gmail.com>
2020-08-24 22:40:33 -0400

    clear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,42 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TareaComponent } from './tarea/tarea.component';
 import { FormsModule } from '@angular/forms';
-import { PruebaComponent } from './components/prueba/prueba.component';
-import { CambiaridiomaPipe } from './cambiaridioma.pipe';
-import { PersonaModule } from './modules/persona/persona.module';
-import { ProductoModule } from './modules/producto/producto.module';
-import { AdminModule } from './modules/admin/admin.module';
-import { UserModule } from './modules/user/user.module';
-import { PipemarianaPipe } from './pipes/pipemariana.pipe';
-import { SquarePipe } from './pipes/square.pipe';
-import { PurePipe } from './pipes/pure.pipe';
-import { ImpurePipe } from './pipes/impure.pipe';
 import { NgColorDirective } from './directives/ng-color.directive';
-import { TestComponent } from './components/test/test.component';
 import { NgFocusDirective } from './directives/ng-focus.directive';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
-    TareaComponent,
-    PruebaComponent,
-    CambiaridiomaPipe,
-    PipemarianaPipe,
-    SquarePipe,
-    PurePipe,
-    ImpurePipe,
     NgColorDirective,
-    NgFocusDirective
+    NgFocusDirective,
+    HomeComponent
   ],
   imports: [
     BrowserModule, 
-    FormsModule,
-    PersonaModule,
-    ProductoModule,
-    AdminModule,
-    UserModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

8d57fa6beda62414010547dbc9784f3defa74abd
mariana <luzmarianacarlo1999@gmail.com>
2020-09-02 21:13:01 -0400

    clear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
-import { Home2Component } from './components/home2/home2.component';
-import { Home1Component } from './components/home1/home1.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -8,19 +7,11 @@ import {MatIconModule} from '@angular/material/icon';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { AdminComponent } from './components/admin/admin.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    AdminComponent,
-    Home1Component,
-    Home2Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
